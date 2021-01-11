f147ca6305103731994b4319373e1fb8570fc8a7
Christian <christian@randleff.dk>
2021-01-11 09:56:31 +0100

    epic gamer moment

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,4 +4,14 @@ public class main extends PApplet {
     public static void main(String[] args) {
         PApplet.main("main");
     }
+
+    @Override
+    public void setup() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
 }
