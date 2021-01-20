f231e6dab5de4d94696e7db2d6948efe64ca4f4a
casp8108 <54103603+casp8108@users.noreply.github.com>
2021-01-20 14:23:21 +0100

    Item.java er en ting, med dens seje variabler, dens seje constructor og sådan det der :DDD

---
+++ src/main/java/Item.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+import javax.swing.text.AbstractDocument;
+
+public class Item {
+    PApplet p;
+    float value;
+    String Name;
+    String Img;
+    int ammount;
+
+    //----------CONSTRUCTOR----------
+    Item(float v,int a, String n, String i){
+        v = value;
+        a = ammount;
+        n = Name;
+        i = Img;
+    }
+    //----------METHODS----------
+    void idk_mand(){
+
+    }
+}
