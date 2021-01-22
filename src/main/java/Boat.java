2dc833e12bf971f324359326cec0b9fb3221585c
casp8108 <54103603+casp8108@users.noreply.github.com>
2021-01-22 08:35:19 +0100

    :DD båd

---
+++ src/main/java/Boat.java
@@ -0,0 +1,23 @@
+import processing.core.PApplet;
+
+import javax.swing.text.AbstractDocument;
+import java.util.ArrayList;
+
+public class Boat {
+    PApplet p;
+    int xPos, yPos;
+    float score, money;
+    ArrayList<Item> inventory;
+
+    //----------CONSTRUCTOR----------
+    Item(float v,int a, String n, String i){
+        v = value;
+        a = ammount;
+        n = Name;
+        i = Img;
+    }
+    //----------METHODS----------
+    void idk_mand(){
+
+    }
+}
