e97dbecd9e7a103a435fd03ac2c6acf270deb296
casp8108 <54103603+casp8108@users.noreply.github.com>
2021-02-03 12:35:28 +0100

    random movement system

--- src/main/java/Cpu.java
+++ src/main/java/Cpu.java
@@ -8,4 +8,14 @@ public class Cpu extends Boat {
     public Cpu(PApplet p, float x, float y, float s, float m, ArrayList<Item> inventory) {
         super(p, x, y, s, m, inventory);
     }
+
+    void Move(){
+        float movementsLeft = 10;
+        float f = p.random(-10,10);
+        xPos = xPos + f;
+        movementsLeft-= f;
+        yPos = yPos + p.random(-movementsLeft,movementsLeft);
+
+
+    }
 }
