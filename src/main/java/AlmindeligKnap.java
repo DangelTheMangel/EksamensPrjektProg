a29818174fb132bd6ff4499a1088b556786c5b87
Christian <christian@randleff.dk>
2021-04-10 17:50:04 +0200

    Nu er der music og kliks vi mangler bare volume knap

--- src/main/java/AlmindeligKnap.java
+++ src/main/java/AlmindeligKnap.java
@@ -1,5 +1,11 @@
 import processing.core.PApplet;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+import java.util.Scanner;
+
 public class AlmindeligKnap extends Knap {
 
     AlmindeligKnap(PApplet papp, int posX, int posY, int sizeX, int sizeY, String text) {
@@ -14,6 +20,19 @@ public class AlmindeligKnap extends Knap {
                 mouseX < positionX + sizeX &&
                 mouseY > positionY &&
                 mouseY < positionY + sizeY) {
+            try {
+                File file = new File("Music/k.wav");
+                Scanner scanner = new Scanner(System.in);
+                AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(file);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInputStream);
+                clip.start();
+
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             klikket = true;
         }
     }
